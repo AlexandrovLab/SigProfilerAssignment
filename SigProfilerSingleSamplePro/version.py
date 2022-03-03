ac9ddfd99c24edd86c6bdf95fc55d880dbe2c1ba
Raviteja Vangara <raviteja2vangara@gmail.com>
2022-03-03 09:11:04 -0800

    SigProfilerSingleSamplePro Version 0.0.1

--- SigProfilerSingleSamplePro/version.py
+++ SigProfilerSingleSamplePro/version.py
@@ -1,7 +1,7 @@
 
 # THIS FILE IS GENERATED FROM SIGPROFILERSINGLESAMPLEPRO SETUP.PY
-short_version = '1.1.4'
-version = '1.1.4'
-Update = 'Heirarchy option deleted, clustering deleted and signatures orders by the mutation burden'
+short_version = '0.0.1'
+version = '0.0.1'
+Update = 'New Repository'
     
     
