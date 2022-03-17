d54e6a87574acc9fa96a9c1d45566bb349d24a97
Raviteja Vangara <raviteja2vangara@gmail.com>
2022-03-16 17:21:04 -0700

    version change

--- SigProfilerAssignment/version.py
+++ SigProfilerAssignment/version.py
@@ -1,7 +1,7 @@
 
-# THIS FILE IS GENERATED FROM SIGPROFILERSINGLESAMPLEPRO SETUP.PY
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
 short_version = '0.0.1'
 version = '0.0.1'
-Update = 'New Repository'
+Update = 'updating Repository'
     
     
