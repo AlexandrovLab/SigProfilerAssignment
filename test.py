e75a2a96f7caefc77c10de3e5e3819f77ed0b00b
Raviteja Vangara <raviteja2vangara@gmail.com>
2022-06-05 13:45:47 -0700

    Updates to version 0.0.6

--- test.py
+++ test.py
@@ -9,26 +9,25 @@ import SigProfilerAssignment as spa
 from SigProfilerAssignment import Analyzer as Analyze
 dir_inp = spa.__path__[0]+'/data/Examples/'
 def main():
-    #signatures=dir_inp+"SBS288_7_Signatures/Signatures/SBS288_S7_Signatures.txt"
-    # samples=dir_inp+"SBS288_7_Signatures/Samples.txt"
-    signatures=dir_inp+"SBS1536_5_Signatures/Signatures/SBS1536_S5_Signatures.txt"
-    samples=dir_inp+"SBS1536_5_Signatures/Samples.txt"
+
+    signatures  = dir_inp+"Results_scenario_8/SBS96/All_Solutions/SBS96_3_Signatures/Signatures/SBS96_S3_Signatures.txt"
+    samples     = dir_inp+"Input_scenario_8/Samples.txt"
     output="output_example/"
-    # sigs= "COSMIC_v3_SBS_GRCh37_noSBS84-85.txt"
+    sigs= "COSMIC_v3_SBS_GRCh37_noSBS84-85.txt"
 
 
-    signature_subgroups = [#'remove_MMR_deficiency_signatures',
-                            #'remove_POL_deficiency_signatures',
-                            #'remove_HR_deficiency_signatures' ,
-                            #'remove_BER_deficiency_signatures',
-                            'remove_Chemotherapy_signatures', 
-                            'remove_APOBEC_signatures', 
-                            #'remove_Tobacco_signatures', 
-                            #'remove_UV_signatures', 
-                            #'remove_AA_signatures',
-                            'remove_Colibactin_signatures', 
-                            'remove_Artifact_signatures', 
-                            'remove_Lymphoid_signatures']
+    # signature_subgroups = ['remove_MMR_deficiency_signatures',
+    #                         'remove_POL_deficiency_signatures',
+    #                         'remove_HR_deficiency_signatures' ,
+    #                         'remove_BER_deficiency_signatures',
+    #                         'remove_Chemotherapy_signatures', 
+    #                         'remove_APOBEC_signatures', 
+    #                         'remove_Tobacco_signatures', 
+    #                         'remove_UV_signatures', 
+    #                         'remove_AA_signatures',
+    #                         'remove_Colibactin_signatures', 
+    #                         'remove_Artifact_signatures', 
+    #                         'remove_Lymphoid_signatures']
 
     signature_subgroups = None
 
