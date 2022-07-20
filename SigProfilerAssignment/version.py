38f3e50c708177d1dc3abe0b3d102d5dd497a55e
Raviteja Vangara <raviteja2vangara@gmail.com>
2022-07-20 16:14:33 -0700

    Delete version.py

--- SigProfilerAssignment/version.py
+++
@@ -1,7 +0,0 @@
-
-# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
-short_version = '0.0.7'
-version = '0.0.7'
-Update = 'New Release. 1.Bug Fixes with differnt context types 2. Updated documentation 3. Included signature subgroups'
-    
-    
