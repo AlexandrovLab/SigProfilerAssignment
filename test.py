4705d7681e3c0d4bd1874d82cda7e603872ab588
Raviteja Vangara <raviteja2vangara@gmail.com>
2022-08-28 22:38:45 -0700

    VCF input files integration

--- test.py
+++ test.py
@@ -13,9 +13,12 @@ def main():
 
     signatures  = dir_inp+"Results_scenario_8/SBS96/All_Solutions/SBS96_3_Signatures/Signatures/SBS96_S3_Signatures.txt"
     samples     = dir_inp+"Input_scenario_8/Samples.txt"
+    # samples = spa.__path__[0]+'/data/vcftest/' If input is a directory of  vcf files. 
     output="output_example/"
     sigs= "COSMIC_v3_SBS_GRCh37_noSBS84-85.txt"
-
+    
+    
+    # vcf_opts={'project_name': 'test_sample','vcf_context': '288' } # Uncomment this  If  vcf files are provided as input. 
 
     # signature_subgroups = ['remove_MMR_deficiency_signatures',
     #                         'remove_POL_deficiency_signatures',
@@ -39,14 +42,18 @@ def main():
                            genome_build="GRCh37", 
                            verbose=False,
                            new_signature_thresh_hold=0.8,
-                           signature_subgroups=signature_subgroups)
+                           signature_subgroups=signature_subgroups,
+                        #    vcf_opts=vcf_opts
+                           )
 
     Analyze.denovo_fit( samples,
                         output, 
                         signatures=signatures,
                         signature_database=None,
                         genome_build="GRCh37", 
-                        verbose=False)
+                        verbose=False,
+                        # vcf_opts=vcf_opts
+                        )
 
     Analyze.cosmic_fit( samples, 
                         output, 
@@ -56,6 +63,7 @@ def main():
                         verbose=False,
                         collapse_to_SBS96=True,
                         signature_subgroups=signature_subgroups,
+                        # vcf_opts=vcf_opts
                        )
 
 if __name__ == '__main__':
